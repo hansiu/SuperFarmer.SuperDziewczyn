78541b57ea80b666dde4f5c62d1ad933148ba1fe
Aleksandra Dąbrowska <oll.dabrowska@gmail.com>
2016-11-23 16:40:18 +0100

    Poprawki argumentow funkcji
    Poprawki argumentow funkcji

--- R/rzut_kostkami.R
+++ R/rzut_kostkami.R
@@ -2,13 +2,11 @@
 #'
 #'Funkcja rzut_kostkami() wyznacza parę wartosci - nazwy zwierzat na obu kostkach.
 #'
-#'@param a wektor zawierajacy zwierzeta na kostce zielonej
-#'@param b wektor zawierajacy zwierzeta na kostce czerwonej
 #'@return Wektor zawierajacy nazwy zwierzat wylosowanych na kostce zielonej i czerwonej odpowiednio
 #'
 #'@export
 
-rzut_kostkami <- function(a,b){
-  wynik <- c(sample(a, 1), sample(b, 1))
+rzut_kostkami <- function(){
+  wynik <- c(sample(kostka_zielona, 1), sample(kostka_czerwona, 1))
   return(wynik)
 }
