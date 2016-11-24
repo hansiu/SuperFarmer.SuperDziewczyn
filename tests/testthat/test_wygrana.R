522631f04812faa8550218fc0d020add8b6a3e85
hansiu <unchallenged.yo@gmail.com>
2016-11-24 16:35:10 +0100

    testy do wygranej

---
+++ tests/testthat/test_wygrana.R
@@ -0,0 +1,12 @@
+test_that("Wygrywanie przyjmuje dwa wektory i zwraca booleana",{
+  expect_error(wygrana())
+  expect_error(wygrana(c(1,1,1,1,1,0,0)))
+  expect_true(is.logical(wygrana(c(1,1,1,1,1,0,0),c(1,24,0,0,0,4,0))))
+  expect_length(wygrana(c(1,1,1,1,1,0,0),c(1,24,0,0,0,4,0)),1)
+})
+
+test_that("Wygrywanie sprawdza warunki poprawnie",{
+  expect_true(wygrana(c(1,1,1,1,1,0,0),c(1,1,1,1,1,0,0)))
+  expect_false(wygrana(c(1,1,1,1,1,0,0),c(1,1,1,0,3,0,0)))
+  expect_true(wygrana(c(1,1,1,1,1,0,0),c(4,3,3,2,4,2,3)))
+  })
