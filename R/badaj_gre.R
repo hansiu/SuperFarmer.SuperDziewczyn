21b2e771727b5ba20582c0fa7e5e4fd2a90224fd
Aleksandra Dąbrowska <oll.dabrowska@gmail.com>
2016-11-28 14:39:50 +0100

    powtarzanie gry

---
+++ R/badaj_gre.R
@@ -0,0 +1,15 @@
+#'Funkcja powtarzajaca gre 10000 razy
+#'
+#'@param strategia strategia ktora chcemy grac
+#'@return podstawowe statystki dla czasow trwania gier
+#'
+#'@export
+
+badaj_gre <- function(strategia){
+  czasy <- data.frame()
+  for (i in 1:10000){
+    czasy[i,1] <-gra(strategia)
+  }
+  statystyki <- summary(czasy)
+  return(statystyki)
+}
