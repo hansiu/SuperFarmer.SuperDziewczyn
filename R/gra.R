4864696e88113dcfb6154a2592fe668e00b7bbd3
Aleksandra Dąbrowska <oll.dabrowska@gmail.com>
2016-11-28 11:01:28 +0100

    poprawka gry

--- R/gra.R
+++ R/gra.R
@@ -8,15 +8,20 @@
 gra <- function(strategia){
   stado <- numeric(7)
   names(stado) <- c('krolik','owca','swinia','krowa','kon','maly_pies','duzy_pies')
+  stado_max <- numeric(7)
+  names(stado_max) <- c('krolik','owca','swinia','krowa','kon','maly_pies','duzy_pies')
+  stado_max <- c(60,24,20,12,6,4,2)
   licznik <- 0
   warunek_wygranej <- c(1,1,1,1,1,0,0)
   names(warunek_wygranej) <- names(stado)
+  kostka_zielona <- c("krolik","krolik","krolik","krolik","krolik","krolik","owca",   "owca" ,"owca","swinia","krowa","wilk")
+  kostka_czerwona <- c("krolik","krolik","krolik","krolik","krolik","krolik","owca","owca","swinia","swinia","kon", "lis")
   while(wygrana(warunek_wygranej,stado)!=TRUE){
-  wymiana <- strategia(stado)
-  stado <- dokonanie_wymian(stado,stado_max,wymiana)
-  stado <- przebieg_rzutu(stado)
-  cat(stado,"\n")
-  licznik <- licznik+1
+    wymiana <- strategia(stado)
+    stado <- dokonanie_wymian(stado,stado_max,wymiana)
+    stado <- przebieg_rzutu(stado,stado_max,kostka_zielona,kostka_czerwona)
+    cat(stado,"\n")
+    licznik <- licznik+1
   }
   return(licznik)
 }
