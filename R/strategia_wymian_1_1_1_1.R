2acfca3129dec7f1164f36061698e6c27f4f78f2
Aleksandra Dąbrowska <oll.dabrowska@gmail.com>
2016-11-28 11:16:25 +0100

    moja strategia wymian

---
+++ R/strategia_wymian_1_1_1_1.R
@@ -0,0 +1,40 @@
+#'Strategia wymian z zalozeniem ze zawsze chcemy miec jednego krolika, jedna owce, jednego malego psa i jednego duzego psa
+#'
+#'@param stado stado gracza
+#'@return wymiany ze strategii
+#'
+#'@export
+
+strategia_wymian_1_1_1_1 <- function(stado){
+  wymiana<-numeric(7)
+  names(wymiana) <-c("krolik","owca","swinia", "krowa", "kon", "maly_pies","duzy_pies")
+  kroliki <- 1
+  owce <-1
+  male_psy <- 1
+  duze_psy <-1
+
+  wartosc_w_krolikach <- numeric(7)
+  names(wartosc_w_krolikach) <- c('krolik','owca','swinia','krowa','kon','maly_pies','duzy_pies')
+  wartosc_w_krolikach <- c(1,6,12,36,72,6,36)
+
+  if(stado[["kon"]]>= 2){
+    wymiana[1:7]<-c(1,1,1,1,-1,0,0)
+    return(wymiana)
+  }
+  if((stado_w_krolikach(wartosc_w_krolikach,stado[c(1,2,3,4,6,7)])>=72)&&(stado[["kon"]]==1)){
+    wymiana <- kilka_na_jedno(wartosc_w_krolikach,stado,"kon")
+    return(wymiana)
+  }
+  damy<-c( max(stado[["krolik"]]-kroliki,0),max(stado[["owca"]]-owce,0),0,0,0,max(stado[["maly_pies"]]-male_psy,0),max(stado[["duzy_pies"]]-duze_psy))
+  names(damy) <-  c("krolik","owca","swinia", "krowa", "kon", "maly_pies","duzy_pies")
+  for(zw in c("kon","maly_pies","duzy_pies","owca")){
+    wymiana <-kilka_na_jedno(wartosc_w_krolikach,damy,zw)
+    param = switch(zw,"kon" = 10,
+                   "maly_pies" = male_psy,
+                   "duzy_pies" = duze_psy,
+                   "owca" = owce)
+    if(class(wymiana)=="numeric" && stado[[zw]]< param)  return(wymiana)
+  }
+  wymiana[1:7]<-c(0,0,0,0,0,0,0)
+  return(wymiana)
+}
