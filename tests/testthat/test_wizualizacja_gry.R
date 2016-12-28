ab286ff946a7f1cd8c92f6291ecec251c1654835
Aleksandra Dąbrowska <oll.dabrowska@gmail.com>
2016-12-28 14:08:19 +0100

    wizualizacje

--- tests/testthat/test_wizualizacja_gry.R
+++ tests/testthat/test_wizualizacja_gry.R
@@ -1,10 +1,10 @@
-test_that("Wizualizacja gry przyjmuje wektor czasow trwania poszczegolnych gier",{
+test_that("Wizualizacja gry przyjmuje wektor czasow trwania poszczegolnych gier oraz typ wykresu",{
           wektor <- c(20,30,30,20,30,20,10,12)
           expect_error(wizualizacja_gry())
-          expect_length(wizualizacja_gry(wektor),9)
+          expect_length(wizualizacja_gry(wektor,typ_wykresu="histogram"),9)
           })
 
 test_that("Wizualizacja gry zwraca poprawne wartosci",{
   wektor <- c(20,30,30,20,30,20,10,12)
-  expect_is(wizualizacja_gry(wektor),c("gg","ggplot"))
+  expect_is(wizualizacja_gry(wektor,typ_wykresu="histogram"),c("gg","ggplot"))
 })
